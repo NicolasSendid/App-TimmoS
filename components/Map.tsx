d15261b5d5e3d83c760821adef466e695f455a4f
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 15:07:16 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,31 +1,52 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-type Props = {
-  center: [number, number];
-  markers?: { lat: number; lon: number; price: number }[];
+type MarkerType = {
+  lat: number;
+  lon: number;
+  price: number;
+  surface?: number;
 };
 
-export default function Map({ center, markers = [] }: Props) {
+type Props = {
+  center?: [number, number];
+  markers?: MarkerType[];
+};
+
+export default function Map({
+  center = [48.606, 2.45],
+  markers = [],
+}: Props) {
   return (
     <MapContainer
       center={center}
-      zoom={15}
+      zoom={14}
       style={{ height: "400px", width: "100%", marginTop: "20px" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      {/* Marker principal (le bien) */}
+      {/* Bien principal */}
       <Marker position={center}>
-        <Popup>Bien estimé</Popup>
+        <Popup>Bien analysé</Popup>
       </Marker>
+
+      {/* Cercle rayon */}
+      <Circle center={center} radius={500} />
 
       {/* Comparables */}
       {markers.map((m, i) => (
         <Marker key={i} position={[m.lat, m.lon]}>
-          <Popup>Vente : {m.price.toLocaleString()} €</Popup>
+          <Popup>
+            <div>
+              <strong>{m.price.toLocaleString()} €</strong>
+              <br />
+              {m.surface && `${m.surface} m²`}
+              <br />
+              {Math.round(m.price / (m.surface || 1))} €/m²
+            </div>
+          </Popup>
         </Marker>
       ))}
     </MapContainer>
