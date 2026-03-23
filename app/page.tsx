255a652c1dd421f9e12fd1b6daef9620c7698ece
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 10:22:27 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">App-TimmoS</h1>
+      <p>Plateforme immobilière intelligente</p>
+    </div>
+  );
+}
