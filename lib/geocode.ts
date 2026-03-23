fc757a764985b70000a8aa4b063e3a74d8c84c7b
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 14:38:32 +0100

    Update geocode.ts

--- lib/geocode.ts
+++ lib/geocode.ts
@@ -1,11 +1,15 @@
-// lib/geocode.ts
 import axios from "axios";
 
 export async function geocodeAddress(address: string) {
   const res = await axios.get(
     `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(address)}`
   );
-  if (!res.data.features.length) throw new Error("Adresse introuvable");
+
+  if (!res.data.features || res.data.features.length === 0) {
+    throw new Error("Adresse introuvable");
+  }
+
   const [lon, lat] = res.data.features[0].geometry.coordinates;
+
   return { lat, lon };
 }
