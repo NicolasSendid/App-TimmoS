41006fa9b60751273a2e70c0d962cf3d06cd3546
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 10:40:07 +0100

    Update page.tsx

--- app/cadastre/page.tsx
+++ app/cadastre/page.tsx
@@ -1,1 +1,14 @@
+"use client";
 
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("@/components/Map"), { ssr: false });
+
+export default function CadastrePage() {
+  return (
+    <div>
+      <h1>Cadastre</h1>
+      <Map />
+    </div>
+  );
+}
