a03ac4e95928b3e290559a82738dba32770234f5
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 15:13:23 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import L from "leaflet";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
