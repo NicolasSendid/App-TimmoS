7158e6ae7331e9a049a26a85a3060fad95e5b0b9
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 14:55:30 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,33 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function Map() {
-  const position: [number, number] = [48.606, 2.45];
+type Props = {
+  center: [number, number];
+  markers?: { lat: number; lon: number; price: number }[];
+};
 
+export default function Map({ center, markers = [] }: Props) {
   return (
     <MapContainer
-      center={position}
-      zoom={13}
-      style={{ height: "500px", width: "100%" }}
+      center={center}
+      zoom={15}
+      style={{ height: "400px", width: "100%", marginTop: "20px" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} />
+
+      {/* Marker principal (le bien) */}
+      <Marker position={center}>
+        <Popup>Bien estimé</Popup>
+      </Marker>
+
+      {/* Comparables */}
+      {markers.map((m, i) => (
+        <Marker key={i} position={[m.lat, m.lon]}>
+          <Popup>Vente : {m.price.toLocaleString()} €</Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
