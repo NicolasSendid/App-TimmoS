bdf1479db16739c6d49a4401b8918d14892580e3
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 10:21:51 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+export const metadata = {
+  title: "App-TimmoS",
+  description: "Outil immobilier intelligent",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>
+        <Navbar />
+        <main className="p-4">{children}</main>
+      </body>
+    </html>
+  );
+}
