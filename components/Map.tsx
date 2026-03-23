dba496a91b584636362c5b5e4d0cd0fc2d3d033a
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 10:49:07 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13} className="h-[500px]">
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[48.8566, 2.3522]} />
+    <MapContainer
+      center={[48.606, 2.45]}
+      zoom={13}
+      style={{ height: "500px" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[48.606, 2.45]} />
     </MapContainer>
   );
 }
