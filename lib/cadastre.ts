6c409295bec42a63d4026f72d5f36dbc3c9f5a50
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-23 16:27:23 +0100

    Update cadastre.ts

--- lib/cadastre.ts
+++ lib/cadastre.ts
@@ -1,1 +1,12 @@
+export async function getCadastre(lat: number, lon: number) {
+  const res = await fetch(
+    `https://apicarto.ign.fr/api/cadastre/parcelle?geom=${lon},${lat}&source=PCI`
+  );
 
+  const data = await res.json();
+
+  return data.features?.map((f: any) => ({
+    id: f.properties.idu,
+    surface: f.properties.contenance,
+  })) || [];
+}
